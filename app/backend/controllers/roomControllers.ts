9f63d61abaf87020bce67dbc3b7075106771403e
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-01 19:16:36 -0400

    Argument of type xxx is missing the following properties error

--- app/backend/controllers/roomControllers.ts
+++ app/backend/controllers/roomControllers.ts
@@ -1,7 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
+import Room from "../models/room";
 
 export const allRooms = async(req: NextRequest) => {
+    const resPerPage: number = 8;
+
+    const rooms = await Room.find();
+
     return NextResponse.json({
-        data: "hello",
+        success: true,
+        resPerPage,
+        rooms,
+    });
+};
+
+export const newRoom = async (req: NextRequest) => {
+    const body = await req.json();
+
+    const room = await Room.create(body);
+
+    return NextResponse.json({
+        success: true,
+        room,
     });
 };
