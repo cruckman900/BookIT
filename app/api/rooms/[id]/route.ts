a7047ea432872ee7c426eade7ad8c1f9a2d24121
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-02 19:34:35 -0400

    update existing room (working)

--- app/api/rooms/[id]/route.ts
+++ app/api/rooms/[id]/route.ts
@@ -1,5 +1,5 @@
 import dbConnect from '@/app/backend/config/dbConnect'
-import { getRoomDetails } from '@/app/backend/controllers/roomControllers'
+import { getRoomDetails, updateRoom } from '@/app/backend/controllers/roomControllers'
 import { createEdgeRouter } from 'next-connect'
 import { NextRequest } from 'next/server'
 
@@ -14,7 +14,12 @@ const router = createEdgeRouter<NextRequest, RequestContext>();
 dbConnect();
 
 router.get(getRoomDetails);
+router.put(updateRoom);
 
 export async function GET(request: NextRequest, ctx: RequestContext) {
+    return router.run(request, ctx);
+};
+
+export async function PUT(request: NextRequest, ctx: RequestContext) {
     return router.run(request, ctx);
 };
