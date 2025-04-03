09ae43974ba1e1391be947b2f847d0242703136b
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-03 03:26:44 -0400

    created header and footer components

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="py-1 pt-5">
+      <p className="text-center mt-1">Book IT - 2019-2025, All Rights Reserved</p>
+    </footer>
+  )
+}
