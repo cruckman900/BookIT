fb7fc80ffddd08af2bef32a896282223ee5f2fd3
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-03 01:44:57 -0400

    added filter functionaity

---
+++ app/backend/utils/apiFilters.ts
@@ -0,0 +1,33 @@
+class APIFilters {
+    query: any;
+    queryStr: any;
+
+    constructor(query: any, queryStr: any) {
+        this.query = query;
+        this.queryStr = queryStr;
+    };
+
+    search(): APIFilters {
+        const location = this.queryStr?.location 
+        ? {
+            address: {
+                $regex: this.queryStr.location,
+                $options: "i",
+            },
+        }: {};
+
+        this.query = this.query.find({ ...location });
+        return this;
+    };
+
+    filter(): APIFilters {
+        const queryCopy = { ...this.queryStr };
+
+        const removeFields = ['location'];
+        removeFields.forEach((el) => delete queryCopy[el]);
+
+        return this;
+    }
+};
+
+export default APIFilters;
