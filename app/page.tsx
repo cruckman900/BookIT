606415938d3091cf3855fb4f93b3db5b1259bce6
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-04 22:13:15 -0400

    testing caching and revalidation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 import { Home } from "./components/layout/Home";
 
-export default function HomePage() {
-  return (
-    <Home />
-  );
+export const dynamic = "force-dynamic";
+
+const getRooms = async () => {
+  const res = await fetch('http://localhost:3000/api/rooms', {
+    next: {
+      tags: ['Rooms'],
+    }
+  });
+  return res.json();
+}
+
+export default async function HomePage() {
+  const rooms = await getRooms();
+  console.log(rooms);
+
+  return ( <Home /> );
 }
