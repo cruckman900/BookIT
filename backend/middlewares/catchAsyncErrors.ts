6eafd1aeb41696958050e8b0856ddb52981bbba4
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-27 21:47:52 -0400

    custom error message for mongoose  duplicate key

--- backend/middlewares/catchAsyncErrors.ts
+++ backend/middlewares/catchAsyncErrors.ts
@@ -22,6 +22,11 @@ export const catchAsyncErrors = (handler: HandlerFunction) => async(req: NextReq
             error.statusCode = 400;
         }
 
+        // Handling mongoose duplicate key error
+        if (error.code === 11000) {
+            error.message = `Duplicate ${Object.keys(error.keyValue)} entered`;
+        }
+
         return NextResponse.json(
             {
                 errMessage: error.message,
