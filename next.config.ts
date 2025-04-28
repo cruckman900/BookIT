62f3ff2edcbb0c9be72709cbcbe72ace394dae1b
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-28 01:41:16 -0400

    set up cloudinary

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
     API_URL: "http://localhost:3000",
     DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit",
     DB_URI: "",
+
+    CLOUDINARY_CLOUD_NAME: "dqpon9vyj",
+    CLOUDINARY_API_KEY: "328692149314358",
+    CLOUDINARY_API_SECRET: "foP10jIBux896hfSzphuYZ70aYw",
     
     NEXTAUTH_URL: "http://localhost:3000",
     NEXTAUTH_SECRET: "KSDFJKLSDJFLKSDFJSLDKF934KJLDJGDLKGFJDF",
