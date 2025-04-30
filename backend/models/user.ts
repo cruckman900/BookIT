9132dab38a435d5f1dcd2cdbfb7355ec0338e5e1
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-30 19:31:40 -0400

    set up reset password token and expiry

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,5 +1,6 @@
 import mongoose, { Document, Schema } from "mongoose";
 import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
 
 export interface IUser extends Document {
     name: string;
@@ -60,6 +61,20 @@ userSchema.pre('save', async function (next) {
 // Compare user password
 userSchema.methods.comparePassword = async function(enteredPassword: string): Promise<boolean> {
     return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate reset password token
+userSchema.methods.getResetPasswordToken = function(): string {
+    // Generate the token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash the token
+    this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest('hex');
+
+    // Set token expire time
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+    return resetToken;
 };
 
 export default mongoose.models.User || mongoose.model<IUser>("User", userSchema);
