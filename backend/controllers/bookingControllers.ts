b592274212f40d6a56751cd62eeba75bb9c0521a
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-05-23 10:22:31 -0400

    controller function and backend route for check room booking availability

--- backend/controllers/bookingControllers.ts
+++ backend/controllers/bookingControllers.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors";
-import Booking from "../models/booking";
+import Booking, { IBooking } from "../models/booking";
 
+// Create new Booking  =>  /api/bookings
 export const newBooking = catchAsyncErrors(async (req: NextRequest) => {
     const body = await req.json();
 
@@ -27,5 +28,28 @@ export const newBooking = catchAsyncErrors(async (req: NextRequest) => {
 
     return NextResponse.json({
         booking,
+    });
+});
+
+// Check Room Booking Availability  =>  /api/bookings/check
+export const checkRoomBookingAvailability = catchAsyncErrors(async (req: NextRequest) => {
+    const { searchParams } = new URL(req.url);
+    const roomId = searchParams.get("roomId");
+
+    const checkInDate: Date = new Date(searchParams.get("checkInDate") as string);
+    const checkOutDate: Date = new Date(searchParams.get("checkOutDate") as string);
+
+    const bookings: IBooking[] = await Booking.find({
+        room: roomId,
+        $and: [
+            { checkInDate: { $lte: checkOutDate} },
+            { checkOutDate: { $gte: checkInDate} },
+        ],
+    });
+
+    const isAvailable: boolean = bookings.length === 0;
+
+    return NextResponse.json({
+        isAvailable,
     });
 });
